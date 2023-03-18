955c029bbcda427610f81fd761fd3cc2e11a1d3d
Mattia Evangelisti (4G) <MATTIA.EVANGELIST.4g@local.ittcesena.it>
2023-03-18 08:56:54 +0100

    Aggiunta variabili

---
+++ ProjectScuola/alunno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammaScuola
+{
+    internal class alunno
+    {
+        private string nome, cognome;
+        private int anni;
+
+        public string toString()
+        { return ("Nome: " + nome + ", cognome: " + cognome + ", età: " + anni.ToString()); }
+
+        public string classi()
+        {
+            string anno = "";
+
+            if (anni < 16)
+            { anno = "Biennio"; }
+            else
+            { anno = "Triennio"; }
+
+            return anno;
+        }
+
+    }
+}
