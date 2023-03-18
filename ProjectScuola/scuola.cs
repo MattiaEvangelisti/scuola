5957b7c3876a8806e8f91d69365a44eb5e4b1ce8
Mattia Evangelisti (4G) <MATTIA.EVANGELIST.4g@local.ittcesena.it>
2023-03-18 09:25:04 +0100

    fix

---
+++ ProjectScuola/scuola.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ProgrammaScuola
+{
+    internal class scuola
+    {
+        private string nome, indirizzo;
+        private DateTime orarioApertura, orarioChiusura;
+
+        public string insertAlunno(string nome, string cognome, string anni)
+        {
+            alunno alunno = new alunno(nome, cognome, anni);
+            return nome;
+        }
+        public string ricercaAlunno()
+        {
+            return alunno;
+        }
+
+        public int numAlunni()
+        {
+            int i = 0;
+            return i;
+        }
+    }
+}
